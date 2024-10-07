acc6410bd4fe217d4e460bcef2793d6465bbfd01
RajRathinam <rajrathinam2005@gmail.com>
2024-10-07 07:07:39 -0700

    21 commit

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,5 +1,6 @@
-{
-    "presets"; ["react-app"],
-    "plugins"; ["@babel/plugin-proposal-private-property-in-object"]
-  }
+module.exports = {
+    presets: ['react-app'],
+    plugins: ['@babel/plugin-proposal-private-property-in-object'],
+    ignore: ['src/index.js'],
+  };
   
